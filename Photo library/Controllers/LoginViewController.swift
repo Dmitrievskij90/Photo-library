2540441642bdb08a19ae3eba6a270ba3649eac9f
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-05-05 00:19:44 +0600

    add user data validation and LibraryViewController

--- Photo library/Controllers/LoginViewController.swift
+++ Photo library/Controllers/LoginViewController.swift
@@ -8,19 +8,52 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+
+    private let userlogin = "Konstantin"
+    private let userPassword = "20051990"
+
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var verifyButton: UIButton!
 
-
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        loginTextField.delegate = self
+        passwordTextField.delegate = self
     }
 
-
-    @IBAction func verifyButtonPressed(_ sender: Any) {
-    }
-    @IBAction func backButtonPressed(_ sender: Any) {
+    @IBAction private func verifyButtonPressed(_ sender: Any) {
+        validateCredentials()
     }
 
+    @IBAction private func backButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    private func validateCredentials() {
+        guard let login = loginTextField.text else {
+            return
+        }
+        guard let password = passwordTextField.text else {
+            return
+        }
+
+        if login == userlogin, password == userPassword {
+            let viewController = LibraryViewController.instantiate()
+            //            viewController.modalTransitionStyle = .coverVertical
+            //            viewController.modalPresentationStyle = .fullScreen
+            present(viewController, animated: true, completion: nil)
+        } else {
+            presentOneButtonAlert(withTitle: "Error", message: "Wrong user data. Please try again")
+        }
+    }
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        textField.endEditing(true)
+        return true
+    }
 }
