4d5e213bba8ae9c6ceb57987a98a8024a987a48b
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-05-23 16:48:31 +0600

    add imageView to custom cell

--- Photo library/VIew/CustomCollectionViewCell.swift
+++ Photo library/VIew/CustomCollectionViewCell.swift
@@ -9,9 +9,9 @@ import UIKit
 
 class CustomCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
 }
