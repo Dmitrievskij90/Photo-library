d4fa73e51af36a9e10409849d34cd8c2ab238f10
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-05-28 00:15:11 +0600

    add FullScreenPhotoViewController

---
+++ Photo library/Controllers/FullScreenPhotoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FullScreenPhotoViewController.swift
+//  Photo library
+//
+//  Created by Konstantin Dmitrievskiy on 28.05.2021.
+//
+
+import UIKit
+
+class FullScreenPhotoViewController: UIViewController {
+    @IBOutlet weak var fullImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
