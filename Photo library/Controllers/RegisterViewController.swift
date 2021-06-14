d46cdc3d412254b30e0d128ea4a5e02430e67e67
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-06-14 16:10:29 +0600

    save user data with KeychainManager

--- Photo library/Controllers/RegisterViewController.swift
+++ Photo library/Controllers/RegisterViewController.swift
@@ -5,25 +5,60 @@
 //  Created by Konstantin Dmitrievskiy on 14.06.2021.
 //
 
+import KeychainAccess
 import UIKit
 
 class RegisterViewController: UIViewController {
+    private let keychain = Keychain()
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginTextField.delegate = self
+        passwordTextField.delegate = self
+    }
 
-        // Do any additional setup after loading the view.
+    @IBAction private func backButtonPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction private func registerButtonPressed(_ sender: Any) {
+        setUserData()
     }
-    */
 
+    private func setUserData() {
+        guard let login = loginTextField.text else {
+            fatalError("Wrong login")
+        }
+        guard let password = passwordTextField.text else {
+            fatalError("Wrong password")
+        }
+
+        if login.isEmpty || password.isEmpty {
+            presentOneButtonAlert(withTitle: "Empty field", message: "Please enter user data")
+        } else {
+            KeychainManager.shared.setUserCredentials(login: login, password: password)
+            presentLibraryViewController()
+        }
+    }
+
+    private func presentLibraryViewController() {
+        let viewController = LibraryViewController.instantiate()
+        viewController.modalTransitionStyle = .coverVertical
+        viewController.modalPresentationStyle = .fullScreen
+        present(viewController, animated: true, completion: nil)
+    }
+}
+
+extension RegisterViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        textField.endEditing(true)
+        return true
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 }
